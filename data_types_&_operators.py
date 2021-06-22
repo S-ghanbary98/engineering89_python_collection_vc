a8f2c4debbfdd825109a4d247fbc2021dae56b3f
S_ghanbary98 <67980207+Sherv1360@users.noreply.github.com>
2021-06-22 12:00:50 +0100

    Initial commit

---
+++ data_types_&_operators.py
@@ -0,0 +1,37 @@
+# What are Data Types and Operators?
+#Boolean, Numbers, and strings.
+# Operators: Arithmetic Operators
+
+# + - * % /
+# subtraction: -
+# Multiplication: *
+#Addition: +
+# Division: /
+#Modulus: %
+
+
+
+#Comparison Operators
+
+# >
+# <
+# >=
+# <=
+# ==
+# !=
+
+number_1 = 4
+number_2 = 2
+
+print(number_1 + number_2) #6
+print(number_1 - number_2) #2
+print(number_1 * number_2) #8
+print(number_1 / number_2) #2
+print(number_1 % number_2) #0
+print(number_1 ** number_2) #16
+
+
+
+print(number_1 == number_2) #False
+print(number_1 != number_2) #True
+
---
+++ variables.py
@@ -0,0 +1,46 @@
+
+
+print("Hello, Shervin!")
+
+# print function display a string.
+# Variables?
+# what are variables?
+# Variables are placeholders  to store data.
+# the syntax to create a variable, name of variable then equal sign then the value of variable
+# Follow good logical naming convention
+
+
+First_Name = "Shervin"
+Last_Name = "Ghanbary"
+
+#Let's create a variable to store an integer value
+
+Salary = 23000.00   #float value
+Age = 22        #int value
+
+
+print(First_Name + " " +  Last_Name +  " is " + str(Age) + " years old and makes £" + str(Salary))
+
+#type() function helps us find the type of a variable
+
+print(type(Age))
+print(type(First_Name))
+print(type(Salary))
+
+print(Age + 1)
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+first_name = input("What is your first name?")
+last_name = input("What is your last name?")
+age = input("How old are you?")
+DOB = input("What is your date of birth?")
+
+print("First Name: " + first_name)
+print("Last Name: " + last_name)
+print("Date Of Birth: " + DOB)
+print("Age: " + str(age))
+
+print("Hi " + first_name + " " + last_name + ". So you were born on " + DOB + " which makes you "+ str(age) + ". It's very nice to meet you and welcome to python. ")
+
+
+
